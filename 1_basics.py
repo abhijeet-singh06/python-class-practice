176217c74bb267b2e572e8f450fe939e32e32bdf
Abhijeet Singh <viraj555singh@gmail.com>
2026-02-09 13:30:44 +0000

    syntax and loops

---
+++ 1_basics.py
@@ -0,0 +1,77 @@
+# count = 0
+# while count < 5:
+#     print(count)
+#     count += 1
+
+# print numbers from 0 to 10 using a for loop
+
+# for i in range(11):
+#     print(i)    
+
+
+# sum=0
+# i=0
+# while i<11:
+#     sum=sum+i
+#     i=i+1
+# print(sum)
+    
+
+
+# multiplication table of a given number
+# number = int(input("Enter a number: "))
+# for i in range(1, 11):
+#     print(f"{number} x {i} = {number * i}")
+
+
+
+
+# number = int(input("enter the number:"))
+# fact=1
+# i=1
+# while i<=number:
+#     fact=fact*i
+#     i=i+1
+# print(fact)
+
+
+
+# number = int(input("enter the number:"))
+# rev=0
+# while number>0:
+#     rem=number%10
+#     rev=rev*10+rem
+#     number=number//10
+# print(rev)
+
+
+# number = int(input("enter the number:"))
+# a =0
+# b=1
+# i=1
+# print(a)
+# print(b)
+# for i in range(number+1):
+
+#     c=a+b
+#     print(c)
+#     a=b
+#     b=c
+    
+
+# for i in range(1,5):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+        
+
+
+for i in range(1,5):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+
+    
+
+
+
