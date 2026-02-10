4d25af21bd1b2394cea3f81cbba6915e0fc22338
Abhijeet Singh <viraj555singh@gmail.com>
2026-02-10 04:09:15 +0000

    basic ds

---
+++ 2_ds.py
@@ -0,0 +1,79 @@
+# list = [1, 2, 3, 4, 5]
+# print(list[0])
+# print(list[-1])
+# print(len(list))
+
+
+# list = [1, 2, 3, 4, 5]
+# print(sum(list))
+# print(sum(list)/len(list))
+
+
+# list = ['apple','banana','orange','guava']
+# list.append('grapes')
+# list.insert(2,'berry')
+# list.remove('apple')
+# list.pop()
+# print(list)
+
+
+
+# list=[1, 2, 2, 3, 2, 5]
+# print(list.count(2))
+
+
+
+# list=[1, 2, 2, 3, 2, 5]
+# x = list.count(2)
+# if(x>0):
+#     print("present")
+# else:
+#     print("not present")
+
+
+
+# list = [1, 3, 2, 4, 5]
+# print(list.index(3))
+
+# list.sort()
+# print(list)
+
+# list.sort(reverse=True)
+# print(list)
+
+# list.reverse()
+# print(list)
+
+
+
+
+# tuple = (1, 2, 3, 4, 5)
+# print(tuple[0])
+# print(tuple[-1])
+
+# print(len(tuple))
+
+
+# tuple = ('apple','banana','orange','guava')
+# for i in tuple:
+#     print(i)
+    
+    
+# tuple = (1, 2, 2, 4, 5)
+# x=tuple.count(2)
+# if(x>0):
+#     print("present")
+# else:
+#     print("not-present")
+    
+    
+# tuple1 = (1, 2, 2, 4, 5)
+# tuple2 = (1, 8, 9, 7, 6)
+# y = tuple1+tuple2
+# print(y)
+
+
+
+
+# tuple = (1, 2, 2, 4, 5)
+# print(tuple.count(2))
